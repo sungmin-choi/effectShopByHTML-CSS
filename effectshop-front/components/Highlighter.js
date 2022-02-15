7162d9dbe74c5abe171d8c6fbf2283d75bdbcbb3
sungmin-choi <namja306@naver.com>
2022-02-15 22:27:57 +0900

    Feat) react-copy-to-clipbord 통해서 클립보드 복사 구현

--- effectshop-front/components/Highlighter.js
+++ effectshop-front/components/Highlighter.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import {coldarkCold} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {CopyTwoTone, CopyOutlined} from '@ant-design/icons';
+import styled from 'styled-components';
 
 
+const ClipboardWrapper = styled.div`
+  position: absolute;
+  right: 7px;
+  top:7px;
+  span{
+    display: block;
+    font-size: 2px;
+  }
 
+`
 
 const Highlighter = ({code,language}) => {
+  const [isCopied,setIsCopied] = useState(false);
+  
+  const onCopy = useCallback(()=>{
+    setIsCopied(true);
+    setTimeout(() => {setIsCopied(false)}, 3000);
+  },[])
   return (
-    <SyntaxHighlighter language={language} style={coldarkCold}>
+    <div style={{position:'relative'}}>
+    <ClipboardWrapper>
+    <CopyToClipboard text={code} onCopy={onCopy}>
+    {isCopied ? <CopyTwoTone style={{fontSize:'20px'}}/> : 
+                <CopyOutlined style={{fontSize:'20px'}}/>}
+    </CopyToClipboard>
+    <span>{isCopied ? 'Copied!!':'Copy'}</span>
+    </ClipboardWrapper>
+    <SyntaxHighlighter language={language} >
       {code}
     </SyntaxHighlighter>
+    </div>
   )
 }
 
 export default Highlighter
+
--- effectshop-front/pages/detail/[id].js
+++ effectshop-front/pages/detail/[id].js
@@ -1,9 +1,10 @@
 import React,{useState,useEffect}from 'react'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Divider } from 'antd'
 import styled from 'styled-components'
 import dummyData from '../../dummyData'
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import Highlighter from '../../components/Highlighter'
 const Container = styled.div`
     display: flex;
     justify-content: center;
@@ -36,11 +37,19 @@ const Detail= () => {
   },[router.isReady])
   if(isLoading){
     return (
+      <>
+      <Head>
+      <title>Effect Shop | please wait</title>
+      </Head>
       <h1>Loading</h1>
+      </>
     )
   }else{
     return (
       <div>
+      <Head>
+        <title>Effect Shop | {payLoad.title}</title>
+      </Head>
       <Divider plain ><h1>{payLoad.title}</h1></Divider>
       <Container data={payLoad}>
       <div className="effect-container"  dangerouslySetInnerHTML={{__html:payLoad.html}}></div>
@@ -48,13 +57,9 @@ const Detail= () => {
       <Divider/>
       <CodeContainer>
       <h1>HTML</h1>
-      <SyntaxHighlighter language='html' >
-        {payLoad.html}
-      </SyntaxHighlighter>
+      <Highlighter code={payLoad.html} language="html"/>
       <h1>CSS</h1>
-      <SyntaxHighlighter language='css' >
-        {payLoad.css}
-      </SyntaxHighlighter>
+      <Highlighter code={payLoad.css} language="css"/>
       </CodeContainer>
       </div>
       )
