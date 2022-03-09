068f5f9d85371b4b3ae833e9d52246ff46294b6e
sungmin-choi <namja306@naver.com>
2022-03-09 16:21:29 +0900

    Fix) 검색후 메인페이지 돌아오기 기능 구현

--- effectshop-front/components/Header.js
+++ effectshop-front/components/Header.js
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Input ,Divider} from 'antd';
 import styled from 'styled-components';
 import {screen} from '../styles/styleConstant';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
-import {SEARCH_EFFECTS_REQUEST} from '../reducers/effect'
+import {SEARCH_EFFECTS_REQUEST,FIRST_LOAD_EFFECTS_REQUEST} from '../reducers/effect'
+import { useRouter } from 'next/router';
 
 const DividerWrapper = styled.div`
   width: 90%;
@@ -30,6 +31,7 @@ const HeaderWrapper = styled.header`
   font-family: 'Mochiy Pop P One', sans-serif;
   font-family: 'Rowdies', cursive;
   text-align: center;
+  cursor: pointer;
   .headerTitle{
     color: #303952;
     font-size: 3rem;
@@ -50,19 +52,26 @@ const HeaderWrapper = styled.header`
 
 const Header = () => {
   const {me} = useSelector(state=>state.user);
+  const [keyword,setKeyword] = useState('');
   const dispatch = useDispatch();
-  const onSearch = useCallback((value)=>{
+  const onSearch = useCallback(()=>{
     dispatch({
       type: SEARCH_EFFECTS_REQUEST,
-      data:value
+      data:keyword
     })
-  },[])
+  },[keyword])
+  const gotoHome = () =>{
+    setKeyword('');
+    dispatch({
+      type: FIRST_LOAD_EFFECTS_REQUEST
+    })
+  }
   
   return (
     <>
-    <HeaderWrapper>
+    <HeaderWrapper >
       <div  style={{height:'110px',paddingTop:'2rem',marginBottom:'10px'}}>
-        <h1 className='headerTitle'>Effect Shop By HTML&CSS</h1>
+        <h1 onClick={gotoHome} className='headerTitle'>Effect Shop By HTML&CSS</h1>
       </div>
       <Input.Search
       style={{width:'450px'}}
@@ -70,6 +79,10 @@ const Header = () => {
       allowClear
       enterButton="Search"
       size="large"
+      value={keyword}
+      onChange={(e)=>{
+        setKeyword(e.target.value);
+      }}
       onSearch={onSearch}
       />
       <LinkBox>
