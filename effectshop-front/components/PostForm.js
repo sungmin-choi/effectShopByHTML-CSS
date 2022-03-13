b27dc1abbf1d3391705e1ad0ff655fef2ac650b6
sungmin-choi <namja306@naver.com>
2022-03-13 21:21:36 +0900

    Refact)PostForm 유효성 검사 구현

--- effectshop-front/components/PostForm.js
+++ effectshop-front/components/PostForm.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {Form,Input,Button} from 'antd';
 import { ADD_EFFECTS_REQUEST } from '../reducers/effect';
 import { useDispatch } from 'react-redux';
@@ -9,6 +9,7 @@ const PostForm = () => {
   const [html,setHtml] = useState('');
   const [css,setCss] = useState('');
   const [title,setTitle] = useState('');
+  const [, forceUpdate] = useState({});
   const dispatch = useDispatch();
   const onFinish = (values) =>{
     dispatch({
@@ -19,6 +20,10 @@ const PostForm = () => {
     setCss('');
     setTitle('');
   }
+  const [form] = Form.useForm();
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
 
   const initialHtml = `<div class="body">
   <h1>여기안에서 테스트 해 주세요</h1>
@@ -27,6 +32,7 @@ const PostForm = () => {
   return (
     <div style={{position:'relative'}}>
     <CustomForm
+    form={form}
     onFinish ={onFinish} 
     name="dynamic_rule">
     <label><h3>Title</h3></label>
@@ -42,15 +48,30 @@ const PostForm = () => {
       <Input value={title} onChange={(e)=>setTitle(e.target.value)} placeholder="Please input title" />
     </Form.Item>
     <label><h3>HTML CODE</h3></label>
-    <Form.Item name="html">
+    <Form.Item 
+    name="html"
+    rules={[
+      {
+        required: true,
+      },
+    ]}>
       <Input.TextArea style={{height:'300px'}} placeholder={initialHtml} value={html} onChange={(e)=>setHtml(e.target.value)}/>
     </Form.Item>
     <label><h3>CSS CODE</h3></label>
-    <Form.Item name="css">
+    <Form.Item 
+    name="css"
+    rules={[
+      {
+        required: true,
+      },
+    ]}>
       <Input.TextArea style={{height:'300px'}} value={css} onChange={(e)=>setCss(e.target.value)}/>
     </Form.Item>
     <Form.Item >
-      <Button type="primary" htmlType="submit">
+      <Button disabled={
+      !form.isFieldsTouched(true) ||
+      !!form.getFieldsError().filter(({ errors }) => errors.length).length
+    } type="primary" htmlType="submit">
         생성하기
       </Button>
     </Form.Item>
