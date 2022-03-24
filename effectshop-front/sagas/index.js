a9d9afbc632b7e65d37bff507bb6bfed2bd29aa6
sungmin-choi <namja306@naver.com>
2022-03-24 18:20:56 +0900

    style) item 반응형 시 y-overflow 수정

--- effectshop-front/sagas/index.js
+++ effectshop-front/sagas/index.js
@@ -2,7 +2,7 @@ import {all,fork} from 'redux-saga/effects';
 import userSaga from './user';
 import effectSaga from './effect';
 import axios from 'axios';
-axios.defaults.baseURL = process.env.NODE_ENV==="development"? "https://api.effectshop-htmlcss.ml":"http://localhost:3065"
+axios.defaults.baseURL = "https://api.effectshop-htmlcss.ml";
 axios.defaults.withCredentials=true;
 export default function* rootSaga(){
     yield all([
